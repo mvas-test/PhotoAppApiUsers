d47f84b4f374655073cf4e53cc7a9bf1ea3a5431
Michail Vasilakopoulos <bouzouki2015@gmail.com>
2020-05-23 11:58:11 +0100

    set up a USerController & Eureka client properties

--- src/main/java/com/appsdeveloper/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ src/main/java/com/appsdeveloper/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -2,7 +2,9 @@ package com.appsdeveloper.photoapp.api.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PhotoAppApiUsersApplication {
 
