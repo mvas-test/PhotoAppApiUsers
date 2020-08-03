24a2ec4a717a12c1a3c4603f60dfc6a447e862b1
Michail Vasilakopoulos <bouzouki2015@gmail.com>
2020-08-03 22:17:11 +0100

    Error Handling with Feign Hystrix FallbackFactory

--- src/main/java/com/appsdeveloper/photoapp/api/users/service/clients/AlbumsServiceClient.java
+++ src/main/java/com/appsdeveloper/photoapp/api/users/service/clients/AlbumsServiceClient.java
@@ -3,6 +3,8 @@ package com.appsdeveloper.photoapp.api.users.service.clients;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.appsdeveloper.photoapp.api.users.entities.AlbumResponseModel;
 
-@FeignClient(name="albums-ws", fallback=AlbumsFallback.class)
+import feign.FeignException;
+import feign.hystrix.FallbackFactory;
+
+@FeignClient(name="albums-ws", fallbackFactory=AlbumsFallbackFactory.class)
 public interface AlbumsServiceClient {
 	
 	@GetMapping("/users/{id}/albums")
@@ -18,12 +23,51 @@ public interface AlbumsServiceClient {
 
 }
 
+//@Component
+//class AlbumsFallback implements AlbumsServiceClient {
+//
+//	@Override
+//	public List<AlbumResponseModel> getAlbums(String id) {
+//		return new ArrayList<>();
+//	}
+//
+//}
+
 @Component
-class AlbumsFallback implements AlbumsServiceClient {
+class AlbumsFallbackFactory implements FallbackFactory<AlbumsServiceClient> {
 
 	@Override
-	public List<AlbumResponseModel> getAlbums(String id) {
-		return new ArrayList<>();
+	public AlbumsServiceClient create(Throwable cause) {
+		return new AlbumsServiceFallbackFactoryFallback(cause);
 	}
 
 }
+
+
+class AlbumsServiceFallbackFactoryFallback implements AlbumsServiceClient {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	private final Throwable cause;
+
+	public AlbumsServiceFallbackFactoryFallback(Throwable cause) {
+		this.cause = cause;
+	}
+
+	@Override
+	public List<AlbumResponseModel> getAlbums(String id) {
+		
+		if (cause instanceof FeignException && ((FeignException) cause).status() == 404) {
+			logger.error("404 error took place when getAlbums was called with userId: " 
+						+ id + ", Error message: "
+						+ cause.getLocalizedMessage());
+		} else {
+			logger.error("Other error took place: " + cause.getLocalizedMessage());
+		}
+		
+		return new ArrayList<>();
+	}
+	
+}
+
+ 
