35e356ad61b635a55a0f3fb456aa1bae238f61a8
Michail Vasilakopoulos <bouzouki2015@gmail.com>
2020-08-03 19:43:54 +0100

    Configured Project to use Hystrix Circuit Breaker

--- src/main/java/com/appsdeveloper/photoapp/api/users/service/clients/AlbumsServiceClient.java
+++ src/main/java/com/appsdeveloper/photoapp/api/users/service/clients/AlbumsServiceClient.java
@@ -1,17 +1,29 @@
 package com.appsdeveloper.photoapp.api.users.service.clients;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.appsdeveloper.photoapp.api.users.entities.AlbumResponseModel;
 
-@FeignClient(name="albums-ws")
+@FeignClient(name="albums-ws", fallback=AlbumsFallback.class)
 public interface AlbumsServiceClient {
 	
 	@GetMapping("/users/{id}/albums")
 	public List<AlbumResponseModel> getAlbums(@PathVariable String id);
+
+}
+
+@Component
+class AlbumsFallback implements AlbumsServiceClient {
+
+	@Override
+	public List<AlbumResponseModel> getAlbums(String id) {
+		return new ArrayList<>();
+	}
 
 }
